7e3ae4532c1d959cc9fd0755a1043810390d1580
Mihai Tabara <tabara.mihai@gmail.com>
2014-09-01 00:42:13 -0700

    Add initial Constanta file and Storage Node Metadata Capsule.

---
+++ AmazonDynamoPlayground/src/environment/Constants.java
@@ -0,0 +1,31 @@
+package environment;
+
+/**
+* @author mtabara
+* The constants class used across the project by all entities
+*/
+public final class Constants {
+	
+	/**
+	 * Private Constructor of the constants class
+	 */
+	private Constants() {
+		
+	}
+	
+	/**
+	 * Size of preference list for any key
+	 */
+	public static final int Dynamo_N = 3;
+
+	/**
+	 * Minimum no. of reads to participate in a successful read operation
+	 */
+	public static final int Dynamo_R = 2;
+	
+	/**
+	 * Minimum no. of writes to participate in a successful write operation
+	 */
+	public static final int Dynamo_W = 2;
+	
+}
---
+++ AmazonDynamoPlayground/src/storage/StorageNodeMetadataCapsule.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package storage;
+
+/**
+ * @author mtabara
+ * Class to describe how the metadata looks like
+ */
+public class StorageNodeMetadataCapsule {
+	
+	private static int counter = 0;
+	
+	private int id;
+	private int port;
+	private int position;
+	
+	/**
+	 * @param id
+	 * @param port
+	 * @param position
+	 */
+	public StorageNodeMetadataCapsule(int id, int port, int position) {
+		this.id = ++counter;
+		this.port = port;
+		this.position = position;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the port
+	 */
+	public int getPort() {
+		return port;
+	}
+	/**
+	 * @param port the port to set
+	 */
+	public void setPort(int port) {
+		this.port = port;
+	}
+	/**
+	 * @return the position
+	 */
+	public int getPosition() {
+		return position;
+	}
+	/**
+	 * @param position the position to set
+	 */
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "StorageNodeMetadataCapsule [id=" + id + ", port=" + port
+				+ ", position=" + position + "]";
+	}
+	
+}
