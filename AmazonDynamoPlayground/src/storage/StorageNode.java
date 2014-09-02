7f11ee05d3f691bc46c5b41170f2a668ebf4c278
Mihai Tabara <tabara.mihai@gmail.com>
2014-09-02 00:11:19 -0700

    Failed attempt to play with multiple storage nodes.

--- AmazonDynamoPlayground/src/storage/StorageNode.java
+++ AmazonDynamoPlayground/src/storage/StorageNode.java
@@ -6,20 +6,17 @@ package storage;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.xml.crypto.NodeSetData;
-
 import environment.Constants;
 import environment.Hasher;
 import environment.Mailman;
 import environment.TaskCapsule;
-import loadbalancer.LoadBalancer;
-import loadbalancer.LoadBalancerRunner;
 
 /**
  * @author mtabara
@@ -55,31 +52,42 @@ public class StorageNode {
 			logger.log(Level.SEVERE, e.getMessage(), e);
 		}
 		
+		System.out.println("Intru aici!");
 		this.metadata = new StorageNodeMetadataCapsule(localId, nodeServerSocket.getLocalPort(), localPosition);
+		System.out.println("==>" + this.metadata.toString());
 	}
 	
 	public static void main(String[] args) throws SecurityException, IOException {
-		StorageNode node = new StorageNode();
+		List<StorageNode> nodes = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			StorageNode node = new StorageNode();
+			nodes.add(node);
+		}
 		
-		Handler fh = new FileHandler("%h/storage_node" + node.metadata.getId() + ".log", true);
-		logger.addHandler(fh);
-		logger.setLevel(Level.INFO);
+		System.out.println("XXX: " + nodes.toString());
 		
-		logger.info("Send my metadata to load balancer.");
-		Mailman mailMan = new Mailman(Constants.GENERIC_HOST, Constants.LOAD_BALANCER_RUNNING_PORT);
-		mailMan.composeMail(new TaskCapsule(node.metadata));
-		mailMan.sendMail();
-
-		logger.info("Start listening on port for connections.");
-		while (true) {
-			try {
-				Socket clientSocket = node.nodeServerSocket.accept();
-				logger.fine("Somebody is sending me something");
-				StorageNodeRunner actionRunner = new StorageNodeRunner(node, clientSocket);
-				actionRunner.start();
-				
-			} catch (IOException e) {
-				logger.log(Level.SEVERE, e.getMessage(), e);
+		for (StorageNode node : nodes) {
+			System.out.println("Intru in for aici!");
+			Handler fh = new FileHandler("%h/storage_node" + node.metadata.getId() + ".log", true);
+			logger.addHandler(fh);
+			logger.setLevel(Level.INFO);
+			
+			System.out.println("Send my metadata to load balancer.");
+			Mailman mailMan = new Mailman(Constants.GENERIC_HOST, Constants.LOAD_BALANCER_RUNNING_PORT);
+			mailMan.composeMail(new TaskCapsule(node.metadata));
+			mailMan.sendMail();
+	
+			logger.info("Start listening on port for connections.");
+			while (true) {
+				try {
+					Socket clientSocket = node.nodeServerSocket.accept();
+					logger.fine("Somebody is sending me something");
+					StorageNodeRunner actionRunner = new StorageNodeRunner(node, clientSocket);
+					actionRunner.start();
+					
+				} catch (IOException e) {
+					logger.log(Level.SEVERE, e.getMessage(), e);
+				}
 			}
 		}
 	}
